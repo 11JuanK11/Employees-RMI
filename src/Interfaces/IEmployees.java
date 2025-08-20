37dcd854438c105fdaeafe1358df1275dfa59573
SaraSanchez <sara_sanchez82221@elpoli.edu.co>
2025-08-20 11:52:54 -0500

    Add methods for client funcionality

--- src/Interfaces/IEmployees.java
+++ src/Interfaces/IEmployees.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface IEmployees<T> extends Remote {
     public T randomPayments(T employees, int numMonths) throws RemoteException;
-    public float totalPaidForEmployee(List<T> list) throws RemoteException;
-    public float averageForMonth(List<T> list) throws RemoteException;
-    public float totalPaid(List<T> list) throws RemoteException;
+    public String totalPaidForEmployee(List<T> list) throws RemoteException;
+    public String averageForMonth(List<T> list) throws RemoteException;
+    public String totalPaid(List<T> list) throws RemoteException;
 }
